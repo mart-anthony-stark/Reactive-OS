8d4623c6dc199886b260a22a5931235b89d8b699
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-09-24 01:57:40 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContextMenu from './components/context/ContextMenu'
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <ContextMenu />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/context/ContextMenu.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {AiFillCopy} from 'react-icons/ai'
+import {BiCut, BiPaste, BiRefresh} from 'react-icons/bi'
+import {BsDisplay} from 'react-icons/bs'
+import {RiShutDownLine} from 'react-icons/ri'
+import './context.css'
+
+export default function ContextMenu(){
+
+  React.useEffect(()=>{
+    window.addEventListener('contextmenu', handleContext)
+    window.addEventListener('click', handleClose)
+    return ()=> window.removeEventListener('contextmenu', handleContext)
+  },[])
+
+  function handleContext(e){
+     e.preventDefault()
+      const menu = document.querySelector('.context-menu')
+      menu.style.top = e.offsetY + 'px'
+      menu.style.left= e.offsetX + 'px'
+      menu.classList.add('open')
+  }
+  function handleClose(e){
+    document.querySelector('.context-menu').classList.remove('open')
+  }
+
+  return(
+    <div className='context-menu'>
+      <div className='item'>
+        <BiRefresh/><span>Refresh</span>
+      </div>
+      <div className='item'>
+        <BiCut/><span>Cut</span>
+      </div>
+      <div className='item'>
+        <AiFillCopy/><span>Copy</span>
+      </div>
+      <div className='item'>
+        <BiPaste/><span>Paste</span>
+      </div>
+      <div className='item'>
+        <AiFillCopy/><span>Copy</span>
+      </div>
+      <hr/>
+      
+      <div className='item'>
+        <BsDisplay/><span>Personalize</span>
+      </div>
+      <div className='item'>
+        <RiShutDownLine/><span>Shutdown</span>
+      </div>
+    </div>
+  )
+}
