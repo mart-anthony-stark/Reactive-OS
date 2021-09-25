ec5e8eee1a750fa8af187f0f534c687095e6b16c
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-09-25 08:05:40 +0000

    fixed: updated pointer cursors

---
+++ src/components/clock/Clock.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import './clock.css'
+
+export default function Clock(){
+  const [time, setTime] = useState('3:15');
+
+  useEffect(()=>{
+    setInterval(getTime, 1000)
+  },[]);
+
+  function getTime(){
+    let date = new Date()
+    let h = date.getHours()
+    let m = date.getMinutes()
+    let s = date.getSeconds()
+    let ampm = h>=12 ? 'pm' : 'am'
+
+    if(h==0) h = 12
+    if(h>12) h = h-12
+
+    h = h<10 ? "0"+ h : h
+    m = m<10 ? "0"+ m : m
+    s = s<10 ? "0"+ s : s
+
+    setTime(`${h}:${m}::${s} ${ampm}`)
+  }
+  return (
+    <div className='clock'>
+      {time}
+    </div>
+  )
+}
--- src/components/taskbar/Taskbar.jsx
+++ src/components/taskbar/Taskbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './taskbar.css'
+import Clock from '../clock/Clock'
 import {CgMenuRound} from 'react-icons/cg'
+import {IconContext} from 'react-icons'
 
 export default function Taskbar({start,toggleStart}){
   return(
@@ -8,8 +10,12 @@ export default function Taskbar({start,toggleStart}){
       <div className='start-btn' onClick={()=>{
         toggleStart((prev)=> !prev)
       }}>
-        <CgMenuRound />
+        <IconContext.Provider value={{className: "click-btn" }}>
+          <div><CgMenuRound /></div>
+        </IconContext.Provider>
       </div>
+
+      <Clock />
     </div>
   )
 }
