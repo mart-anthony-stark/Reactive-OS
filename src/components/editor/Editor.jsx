5ac25423a3ea164334e798f4dd0728f82657c352
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-09-28 13:25:37 +0000

    feature: editorjs added tools

--- src/components/editor/Editor.jsx
+++ src/components/editor/Editor.jsx
@@ -1,13 +1,33 @@
 import React, {useState, useEffect} from 'react'
 import EditorJs from 'react-editor-js';
 import CheckList from '@editorjs/checklist';
+import Embed from '@editorjs/embed'
+import Table from '@editorjs/table'
+import Warning from '@editorjs/warning'
+import Image from '@editorjs/image'
+import LinkTool from '@editorjs/link'
+import Quote from '@editorjs/quote'
+import Delimiter from '@editorjs/delimiter'
+import Code from '@editorjs/code'
+import SimpleImage from '@editorjs/simple-image'
 import './editor.css'
 
 export default function Editor(){
   const [data, setData] = useState('as')
   return(
     <div className='word-editor'>
-      <EditorJs data={data} tools={{ checkList: CheckList }} holder='custom'>
+      <EditorJs data={data} tools={{ 
+        checkList: CheckList ,
+        embed: Embed,
+        table: Table,
+        linkTool: LinkTool,
+        image: Image,
+        warning: Warning,
+        quote: Quote,
+        simpleImage: SimpleImage,
+        delimiter: Delimiter,
+        code: Code,
+      }} holder='custom'>
         <div id='custom'></div>
       </EditorJs>
     </div>
