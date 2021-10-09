574a15462748b263924173bdd828d3189e2cef9b
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-10-09 12:34:34 +0000

    fixed(browser): fixed cleanup functions

--- src/components/browser/Browser.jsx
+++ src/components/browser/Browser.jsx
@@ -3,34 +3,38 @@ import {AiFillCloseCircle} from 'react-icons/ai'
 import './browser.css'
 
 export default function Browser({setTasks, tasks}){
-  const browser = useRef()
-  const browserHeader = useRef()
+
   useEffect(()=>{
+    const app = document.querySelector('.browser')
+    const header = document.querySelector('.browser-header')
+
+    function onDrag({movementX, movementY}){
+      let styles = window.getComputedStyle(app)
+      let left = parseInt(styles.left)
+      let top = parseInt(styles.top)
+
+      app.style.top = `${top+movementY}px`
+      app.style.left = `${left+movementX}px`
+    }
+
+    header.addEventListener('mousedown', ()=>{
+      header.addEventListener('mousemove', onDrag)
+    })
+
     document.addEventListener('mouseup', ()=>{
-      browserHeader.current.removeEventListener('mousemove', onDrag)
+      header.removeEventListener('mousemove', onDrag)
     })
 
     return ()=>{
       document.removeEventListener('mouseup', ()=>{
-        browserHeader.current.removeEventListener('mousemove', onDrag)
+        header.removeEventListener('mousemove', onDrag)
       })
     }
   }, [tasks])
 
-  function onDrag({movementX, movementY}){
-    let styles = window.getComputedStyle(browser.current)
-    let left = parseInt(styles.left)
-    let top = parseInt(styles.top)
-
-    browser.current.style.top = `${top+movementY}px`
-    browser.current.style.left = `${left+movementX}px`
-  }
-
   return(
-    <div className='browser' ref={browser} style={{ zIndex: tasks.indexOf('browser')+1 }}>
-      <div className='browser-header app-header' ref={browserHeader} onMouseDown={()=> { 
-        browserHeader.current.addEventListener('mousemove', onDrag)
-      }}>
+    <div className='browser' style={{ zIndex: tasks.indexOf('browser')+1 }}>
+      <div className='browser-header app-header'>
         <span>Browser</span>
         <div className="close-btn" onClick={()=> setTasks((prev)=> prev.filter(p => p !== 'browser'))}>
           <AiFillCloseCircle/>
