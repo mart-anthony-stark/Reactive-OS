88fe5a09fb446a55af8029d92d0c9226fb93032d
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-10-10 12:46:40 +0000

    feature: added twitter profile

--- src/components/twitter/Middle.jsx
+++ src/components/twitter/Middle.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import {FiArrowLeft} from 'react-icons/fi'
+import {HiLink} from 'react-icons/hi'
+import {AiFillCalendar} from 'react-icons/ai'
+import {GiNewBorn} from 'react-icons/gi'
+import BannerImg from './img/twitter-banner.jpg'
+import ProfileImg from './img/dp.jpg'
 import './style.css'
 
 export default function Middle(){
@@ -10,6 +15,30 @@ export default function Middle(){
         <div className='profile'>
           <h3>Mart Anthony</h3>
           <p>105 Tweets</p>
+        </div>
+      </div>
+      <div className='banner'>
+        <img src={BannerImg} />
+      </div>
+      <div className='profile-details'>
+        <div className='edit'><img src={ProfileImg} /><button className='click-btn'>Edit Profile</button></div>
+        <div className='profile'>
+          <h3>Mart Anthony</h3>
+          <p>105 Tweets</p>
+        </div>
+        <div className='bio'>
+          <p>Web DeveloperPersonal computer |</p>
+          <p>BSIT Student |</p>
+          <p>Future Backend Engineer</p>
+          <section>
+            <p><HiLink/><a href='https://twitter.com/anthony_stark21' target="_blank">martanthonysalazar.tech</a></p>
+            <p><GiNewBorn/>Born September 21, 2001</p>
+            <p><AiFillCalendar/>Joined April 2020</p>
+          </section>
+          <div className='followers'>
+            <p><span>148</span> Following</p>
+            <p><span>73</span> Followers</p>
+          </div>
         </div>
       </div>
     </div>
