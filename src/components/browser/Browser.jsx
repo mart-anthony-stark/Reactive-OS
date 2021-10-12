09a7406e51a4c6ebc833efb6c5421eaab9e9ae10
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-10-12 13:19:15 +0000

    (browser) added languages offered

--- src/components/browser/Browser.jsx
+++ src/components/browser/Browser.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect, useRef}  from 'react'
 import {AiFillCloseCircle} from 'react-icons/ai'
+import ProfileImg from './dp.jpg'
+import {CgMenuGridO} from 'react-icons/cg'
 import './browser.css'
 
 export default function Browser({setTasks, tasks}){
@@ -42,6 +44,13 @@ export default function Browser({setTasks, tasks}){
       </div>
 
       <div className='search-engine'>
+        <div className='top-nav'>
+          <h4>Gmail</h4>
+          <h4>Images</h4>
+          <CgMenuGridO/>
+          <img id='profile-image' src={ProfileImg} />
+        </div>
+
         <h2><span>B</span><span>o</span><span>o</span><span>g</span><span>l</span><span>e</span></h2>
         <input type='text' placeholder='Search' />
         <div className='buttons'>
@@ -51,6 +60,10 @@ export default function Browser({setTasks, tasks}){
         <p className='covid'>
           Have questions on COVID-19 vaccine side effects?
           <a href='#' target='_blank'> Find out more</a>
+        </p>
+        <p className='language'>
+          Google offered in: <span>  </span>
+          <a href='#'>Filipino</a> <a href='#'>Cebuano</a>
         </p>
       </div>
     </div>
