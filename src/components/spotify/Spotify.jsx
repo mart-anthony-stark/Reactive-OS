cd8f45a585448c128c98b84331d75afdb20c380a
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-30 21:12:56 +0800

    fixed overlay z index depending on priority task

--- src/components/spotify/Spotify.jsx
+++ src/components/spotify/Spotify.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { AiFillCloseCircle } from "react-icons/ai";
 import "./spotify.css";
 
-const Spotify = ({ setTasks }) => {
+const Spotify = ({ setTasks, tasks }) => {
   return (
-    <div className="spotify">
+    <div className="spotify" style={{ zIndex: tasks.indexOf("spotify") + 1 }}>
       <div className="spotify-header app-header">
         <span>Spotify</span>
         <div
