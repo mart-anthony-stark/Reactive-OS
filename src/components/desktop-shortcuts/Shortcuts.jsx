526732d589c0ce411d7b556bd3cc63a15a234d6b
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-30 20:08:50 +0800

    added desktop shortcuts component

---
+++ src/components/desktop-shortcuts/Shortcuts.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { IconContext } from "react-icons";
+import {
+  FcCommandLine,
+  FcTodoList,
+  FcSearch,
+  FcCalculator,
+} from "react-icons/fc";
+import './shortcuts.css'
+
+const Shortcuts = () => {
+  return (
+    <div className="desktop-shortcuts">
+      <h1>Desktop Shortcut</h1>
+    </div>
+  );
+};
+
+export default Shortcuts;
