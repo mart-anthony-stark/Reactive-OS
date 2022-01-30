92571b302fe5cf51ffe0181c0a3c7564d477b4a3
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-30 20:46:00 +0800

    added spotify ui structure

--- src/components/spotify/Spotify.jsx
+++ src/components/spotify/Spotify.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
 import "./spotify.css";
 
-const Spotify = () => {
-  return <div className="spotify"></div>;
+const Spotify = ({ setTasks }) => {
+  return (
+    <div className="spotify">
+      <div className="spotify-header app-header">
+        <span>Spotify</span>
+        <div
+          className="close-btn"
+          onClick={() => {
+            setTasks((prev) => prev.filter((p) => p !== "spotify"));
+          }}
+        >
+          <AiFillCloseCircle />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Spotify;
