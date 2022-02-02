38dec05d6194cba6fad502b337b79e00ad1bcc15
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-02 21:29:43 +0800

    added icons

--- src/components/spotify/CurrentSong.jsx
+++ src/components/spotify/CurrentSong.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FaRegWindowMaximize } from "react-icons/fa";
 
 const Currentsong = ({ song }) => {
   return (
     <div className="current-song-preview">
-      <img src={song.img} alt="" />
+      <img src={song.img} alt={song.title} />
+      <div>
+        <h5>{song.title}</h5>
+        <span>{song.artist}</span>
+      </div>
+      <div className="icons">
+        <AiOutlineHeart />
+        <FaRegWindowMaximize />
+      </div>
     </div>
   );
 };
