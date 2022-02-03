822d8eb7d6ae5cc4c2789b7fa36f259c8b928f01
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-03 20:50:55 +0800

    fixed icons

--- src/components/spotify/Controls.jsx
+++ src/components/spotify/Controls.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { IoShuffleOutline } from "react-icons/io5";
 import { CgPlayTrackPrev, CgPlayTrackNext } from "react-icons/cg";
-import { FaPlay } from "react-icons/fa";
-import { FiRepeat } from "react-icons/fi";
+import { IoIosPlay, IoIosRepeat } from "react-icons/io";
+import { IconContext } from "react-icons/lib";
 
 const Controls = () => {
   return (
     <div className="controls">
       <div className="top">
-        <IoShuffleOutline />
-        <CgPlayTrackPrev />
-        <FaPlay />
-        <CgPlayTrackNext />
-        <FiRepeat />
+        <IconContext.Provider value={{ className: "control-icon" }}>
+          <IoShuffleOutline />
+        </IconContext.Provider>
+        <IconContext.Provider value={{ className: "control-icon" }}>
+          <CgPlayTrackPrev />
+        </IconContext.Provider>
+        <div className="play-icon">
+          <IconContext.Provider value={{ className: "control-icon play" }}>
+            <IoIosPlay />
+          </IconContext.Provider>
+        </div>
+        <IconContext.Provider value={{ className: "control-icon" }}>
+          <CgPlayTrackNext />
+        </IconContext.Provider>
+        <IconContext.Provider value={{ className: "control-icon" }}>
+          <IoIosRepeat />
+        </IconContext.Provider>
       </div>
       <div className="bot"></div>
     </div>
