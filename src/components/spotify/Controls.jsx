1ba1e2352c22f04f7e5a8bdf11c102a990aa1b1a
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-03 20:33:27 +0800

    added controls icons

--- src/components/spotify/Controls.jsx
+++ src/components/spotify/Controls.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { IoShuffleOutline } from "react-icons/io5";
+import { CgPlayTrackPrev, CgPlayTrackNext } from "react-icons/cg";
+import { FaPlay } from "react-icons/fa";
+import { FiRepeat } from "react-icons/fi";
 
 const Controls = () => {
-    return (
-        <div className="controls">
-            
-        </div> 
-    );
-}
+  return (
+    <div className="controls">
+      <div className="top">
+        <IoShuffleOutline />
+        <CgPlayTrackPrev />
+        <FaPlay />
+        <CgPlayTrackNext />
+        <FiRepeat />
+      </div>
+      <div className="bot"></div>
+    </div>
+  );
+};
 
 export default Controls;
