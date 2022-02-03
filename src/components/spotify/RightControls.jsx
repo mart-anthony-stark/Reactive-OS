71b0ac10d77c0078b59875118bce5b0ade4b724e
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-03 21:08:27 +0800

    added controls icons

--- src/components/spotify/RightControls.jsx
+++ src/components/spotify/RightControls.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { CgMusicSpeaker } from "react-icons/cg";
 import { GiMicrophone, GiSpeaker } from "react-icons/gi";
+import { IconContext } from "react-icons/lib";
 import { RiPlayList2Line } from "react-icons/ri";
 
 const Rightcontrols = () => {
   return (
     <div className="rightControls">
-      <GiMicrophone />
-      <RiPlayList2Line />
-      <CgMusicSpeaker />
-      <GiSpeaker />
+      <IconContext.Provider value={{ className: "right-control-icon" }}>
+        <GiMicrophone />
+      </IconContext.Provider>
+      <IconContext.Provider value={{ className: "right-control-icon" }}>
+        <RiPlayList2Line />
+      </IconContext.Provider>
+      <IconContext.Provider value={{ className: "right-control-icon" }}>
+        <CgMusicSpeaker />
+      </IconContext.Provider>
+      <IconContext.Provider value={{ className: "right-control-icon" }}>
+        <GiSpeaker />
+      </IconContext.Provider>
       <div className="volume"></div>
     </div>
   );
