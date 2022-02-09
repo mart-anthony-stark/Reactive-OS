18379fbd0eaf2a3dd4b33b1baf1ef248c372d0f7
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-09 19:35:24 +0800

    add: store

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./redux";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
