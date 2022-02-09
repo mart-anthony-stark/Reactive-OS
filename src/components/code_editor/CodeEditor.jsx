22303bc96ffb10e96fe8061e6546d50af1f7a9a0
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-09 20:20:47 +0800

    fixed code editor store mutation

--- src/components/code_editor/CodeEditor.jsx
+++ src/components/code_editor/CodeEditor.jsx
@@ -1,45 +1,49 @@
-import React from 'react'
-import Editor from './Editor'
-import './editor.css'
-import {AiFillCloseCircle} from 'react-icons/ai'
+import React from "react";
+import Editor from "./Editor";
+import "./editor.css";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import { removeTask } from "../../redux/taskSlice";
+import { useDispatch } from "react-redux";
 
-export default function CodeEditor({setTasks, tasks}){
-  React.useEffect(()=>{
-    const editor = document.querySelector('.code-editor')
-    const header = document.querySelector('.code-editor-header')
+export default function CodeEditor() {
+  const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.tasks.tasks);
+  React.useEffect(() => {
+    const editor = document.querySelector(".code-editor");
+    const header = document.querySelector(".code-editor-header");
 
-    header.addEventListener('mousedown', ()=>{
-      header.addEventListener('mousemove', onDrag)
-    })
+    header.addEventListener("mousedown", () => {
+      header.addEventListener("mousemove", onDrag);
+    });
 
-    document.addEventListener('mouseup', ()=>{
-      header.removeEventListener('mousemove', onDrag)
-    })
+    document.addEventListener("mouseup", () => {
+      header.removeEventListener("mousemove", onDrag);
+    });
 
-    return ()=>{
-      header.removeEventListener('mousedown', ()=>{
-        header.addEventListener('mousemove', onDrag)
-      })
+    return () => {
+      header.removeEventListener("mousedown", () => {
+        header.addEventListener("mousemove", onDrag);
+      });
 
-      document.removeEventListener('mouseup', ()=>{
-        header.removeEventListener('mousemove', onDrag)
-      })
-    }
+      document.removeEventListener("mouseup", () => {
+        header.removeEventListener("mousemove", onDrag);
+      });
+    };
+  }, []);
 
-  }, [])
-
-  function onDrag({movementX, movementY}){
-    const editor = document.querySelector('.code-editor')
-    const style = window.getComputedStyle(editor)
-    let top = parseInt(style.top)
-    let left = parseInt(style.left)
-    editor.style.top = `${top + movementY}px`
-    editor.style.left = `${left + movementX}px`
+  function onDrag({ movementX, movementY }) {
+    const editor = document.querySelector(".code-editor");
+    const style = window.getComputedStyle(editor);
+    let top = parseInt(style.top);
+    let left = parseInt(style.left);
+    editor.style.top = `${top + movementY}px`;
+    editor.style.left = `${left + movementX}px`;
   }
 
-  const [html, setHtml] = React.useState('')
-  const [css, setCss] = React.useState('')
-  const [javascript, setJavascript] = React.useState('')
+  const [html, setHtml] = React.useState("");
+  const [css, setCss] = React.useState("");
+  const [javascript, setJavascript] = React.useState("");
 
   const srcDoc = `
     <html>
@@ -47,25 +51,52 @@ export default function CodeEditor({setTasks, tasks}){
       <style>${css}</style>
       <script>${javascript}</script>
     </html>
-  `
+  `;
 
-  return(
-    <div className='code-editor' style={{zIndex: tasks.indexOf('codeeditor')+1}}>
-      <div className='code-editor-header'>
+  return (
+    <div
+      className="code-editor"
+      style={{ zIndex: tasks.indexOf("codeeditor") + 1 }}
+    >
+      <div className="code-editor-header">
         <span>Codear</span>
-        <div className="close-btn" onClick={()=> setTasks((prev)=> prev.filter(p => p !== 'codeeditor'))}>
-          <AiFillCloseCircle/>
+        <div
+          className="close-btn"
+          onClick={() => dispatch(removeTask("codeeditor"))}
+        >
+          <AiFillCloseCircle />
         </div>
       </div>
-      <div className='pane top-pane'>
-        <Editor displayName='HTML' language='xml' onChange={setHtml} value={html} />
-        <Editor displayName='CSS' language='css' onChange={setCss} value={css} />
-        <Editor displayName='JavaScript' language='javascript' onChange={setJavascript} value={javascript} />
-
+      <div className="pane top-pane">
+        <Editor
+          displayName="HTML"
+          language="xml"
+          onChange={setHtml}
+          value={html}
+        />
+        <Editor
+          displayName="CSS"
+          language="css"
+          onChange={setCss}
+          value={css}
+        />
+        <Editor
+          displayName="JavaScript"
+          language="javascript"
+          onChange={setJavascript}
+          value={javascript}
+        />
       </div>
-      <div className='pane preview'>
-        <iframe srcDoc={srcDoc} title='output' sandbox='allow-scripts allow-modals' frameBorder='0' width='100%' height='100%' />
+      <div className="pane preview">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts allow-modals"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
       </div>
     </div>
-  )
+  );
 }
