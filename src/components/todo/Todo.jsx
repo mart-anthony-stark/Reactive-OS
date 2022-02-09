4e041d086f5905527bf8d31fb500b429f3916318
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-09 20:01:56 +0800

    fixed: todo redux

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,74 +1,79 @@
-import React, {useState, useEffect, useRef} from 'react'
-import Task from './Task'
-import {AiFillCloseCircle} from 'react-icons/ai'
-import './todo.css'
+import React, { useState, useEffect, useRef } from "react";
+import Task from "./Task";
+import { AiFillCloseCircle } from "react-icons/ai";
+import "./todo.css";
+import { useSelector } from "react-redux";
 
-export default function Todo({setTasks, tasks}){
-  const [newTask, setNewTask] = useState('')
-  const [todos, setTodos] = useState([{task:'', date:''}])
+export default function Todo({ setTasks }) {
+  const { tasks } = useSelector((state) => state.tasks);
+  const [newTask, setNewTask] = useState("");
+  const [todos, setTodos] = useState([{ task: "", date: "" }]);
 
+  useEffect(() => {
+    setTodos([]);
+    let app = document.querySelector(".todo-app");
+    let header = document.querySelector(".todo-app .todo-header");
 
-  useEffect(()=>{
-    setTodos([])
-    let app = document.querySelector('.todo-app')
-    let header = document.querySelector('.todo-app .todo-header')
-
-    function onDrag({movementX, movementY}){
-      let style = window.getComputedStyle(app)
-      let left = parseInt(style.left)
-      let top = parseInt(style.top)
-      app.style.top = `${top + movementY}px`
-      app.style.left = `${left + movementX}px`
+    function onDrag({ movementX, movementY }) {
+      let style = window.getComputedStyle(app);
+      let left = parseInt(style.left);
+      let top = parseInt(style.top);
+      app.style.top = `${top + movementY}px`;
+      app.style.left = `${left + movementX}px`;
     }
 
-    header.addEventListener('mousedown', ()=>{
-      header.addEventListener('mousemove', onDrag)
-    })
+    header.addEventListener("mousedown", () => {
+      header.addEventListener("mousemove", onDrag);
+    });
 
-    document.addEventListener('mouseup', ()=>{
-      header.removeEventListener('mousemove', onDrag)
-    })
+    document.addEventListener("mouseup", () => {
+      header.removeEventListener("mousemove", onDrag);
+    });
 
+    return () => {
+      header.removeEventListener("mousedown", () => {
+        header.addEventListener("mousemove", onDrag);
+      });
+      document.removeEventListener("mouseup", () => {
+        header.addEventListener("mousemove", onDrag);
+      });
+    };
+  }, []);
 
-    return ()=>{
-      header.removeEventListener('mousedown', ()=>{
-        header.addEventListener('mousemove', onDrag)
-      })
-      document.removeEventListener('mouseup', ()=>{
-        header.addEventListener('mousemove', onDrag)
-      })
-    }
-  }, [])
-
-  function handleNewTask(){
-    if(newTask.length !== 0){
-      setTodos([{task: newTask, date: Date().toLocaleString()}, ...todos])
-      setNewTask('')
+  function handleNewTask() {
+    if (newTask.length !== 0) {
+      setTodos([{ task: newTask, date: Date().toLocaleString() }, ...todos]);
+      setNewTask("");
     }
   }
-  
 
-  return(
-    <div className='todo-app' style={{zIndex: tasks.indexOf('todo')+1}}>
-      <div className='todo-header'>
+  return (
+    <div className="todo-app" style={{ zIndex: tasks.indexOf("todo") + 1 }}>
+      <div className="todo-header">
         <span>TodoList App</span>
-        <div className="close-btn" onClick={()=> setTasks((prev)=> prev.filter(p => p !== 'todo'))}>
-          <AiFillCloseCircle/>
+        <div
+          className="close-btn"
+          // onClick={() => setTasks((prev) => prev.filter((p) => p !== "todo"))}
+        >
+          <AiFillCloseCircle />
         </div>
       </div>
-      <div className='new-task'>
-        <input placeholder='New Task'
-          onChange={(e)=> setNewTask(e.target.value)}
+      <div className="new-task">
+        <input
+          placeholder="New Task"
+          onChange={(e) => setNewTask(e.target.value)}
           value={newTask}
-          onKeyDown={e =>{
-            e.code=='Enter' && handleNewTask()
+          onKeyDown={(e) => {
+            e.code == "Enter" && handleNewTask();
           }}
         />
         <button onClick={handleNewTask}>Add Todo</button>
       </div>
-      <div className='list'>
-        {todos.map((todo, i) => <Task task={todo} key={i} setTodos={setTodos}/>)}
+      <div className="list">
+        {todos.map((todo, i) => (
+          <Task task={todo} key={i} setTodos={setTodos} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
