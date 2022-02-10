cf8ec399a9b5c17328d2f6043e344de24d31cd97
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-10 19:06:26 +0800

    fixed: twitter store mutation

--- src/components/twitter/Twitter.jsx
+++ src/components/twitter/Twitter.jsx
@@ -1,58 +1,64 @@
-import React,{useEffect} from 'react'
-import {AiFillCloseCircle} from 'react-icons/ai'
-import LeftSidebar from './LeftSidebar'
-import Profile from './Profile'
-import RightSide from './RightSide'
-import './style.css'
+import React, { useEffect } from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import LeftSidebar from "./LeftSidebar";
+import Profile from "./Profile";
+import RightSide from "./RightSide";
+import "./style.css";
 
-export default function Twitter({tasks, setTasks}){
-  useEffect(()=>{
-    const app = document.querySelector('.twitter')
-    const header = document.querySelector('.twitter-header')
+export default function Twitter() {
+  const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.tasks.tasks);
+  useEffect(() => {
+    const app = document.querySelector(".twitter");
+    const header = document.querySelector(".twitter-header");
 
-    function onDrag({movementX, movementY}){
-      let styles = window.getComputedStyle(app)
-      let left = parseInt(styles.left)
-      let top = parseInt(styles.top)
+    function onDrag({ movementX, movementY }) {
+      let styles = window.getComputedStyle(app);
+      let left = parseInt(styles.left);
+      let top = parseInt(styles.top);
 
-      app.style.left = `${left + movementX}px`
-      app.style.top = `${top + movementY}px`
+      app.style.left = `${left + movementX}px`;
+      app.style.top = `${top + movementY}px`;
     }
 
-    header.addEventListener('mousedown', ()=>{
-      header.addEventListener('mousemove', onDrag)
-    })
+    header.addEventListener("mousedown", () => {
+      header.addEventListener("mousemove", onDrag);
+    });
 
-    document.addEventListener('mouseup', ()=>{
-      header.removeEventListener('mousemove', onDrag)
-    })
+    document.addEventListener("mouseup", () => {
+      header.removeEventListener("mousemove", onDrag);
+    });
 
-    return ()=>{
-      header.removeEventListener('mousedown', ()=>{
-        header.addEventListener('mousemove', onDrag)
-      })
+    return () => {
+      header.removeEventListener("mousedown", () => {
+        header.addEventListener("mousemove", onDrag);
+      });
 
-      document.removeEventListener('mouseup', ()=>{
-        header.removeEventListener('mousemove', onDrag)
-      })
-    }
-
-  }, [])
-  return(
-    <div className='twitter' style={{zIndex: tasks.indexOf('twitter')+1}}>
-      <div className='twitter-header app-header' >
+      document.removeEventListener("mouseup", () => {
+        header.removeEventListener("mousemove", onDrag);
+      });
+    };
+  }, []);
+  return (
+    <div className="twitter" style={{ zIndex: tasks.indexOf("twitter") + 1 }}>
+      <div className="twitter-header app-header">
         <span>Twitter</span>
-        <div className="close-btn" onClick={()=> {
-          setTasks((prev)=> prev.filter(p => p !== 'twitter'))
-        }}>
-          <AiFillCloseCircle/>
+        <div
+          className="close-btn"
+          onClick={() => {
+            setTasks((prev) => prev.filter((p) => p !== "twitter"));
+          }}
+        >
+          <AiFillCloseCircle />
         </div>
       </div>
-      <div className='hero'>
+      <div className="hero">
         <LeftSidebar />
         <Profile />
-        <RightSide/>
+        <RightSide />
       </div>
     </div>
-  )
+  );
 }
